dd6c3b5fc8f89ac4ecc70be2b04c2930f41cfb07
JunyaLab <junjunnacky1@gmail.com>
2022-11-05 19:41:24 +0900

    とりあえずメモ機能の完成（local storageを使用）

--- src/ui/Memo/Memo.js
+++ src/ui/Memo/Memo.js
@@ -1,19 +1,34 @@
-import { Link } from "react-router-dom";
+import styles from "./Memo.module.css";
 import Header from "../../Components/Header/Header";
 
 const Memo = () => {
   return (
     <>
       <Header />
-      <h1>Memo</h1>
-      <div>
-        <Link to={`/`}>Loginへ</Link>
-      </div>
-      <div>
-        <Link to={`/top/`}>Topへ</Link>
+      <div className={styles["memo-page"]}>
+        <h1 className={styles["memo-title"]}>Memo</h1>
+        <textarea id="memo" className={styles.memo} onClick={edit}>{localStorage.getItem("memo")}</textarea>
       </div>
     </>
   );
 };
 
 export default Memo;
+
+// local storage に変数 memo がなければからの文字列として登録
+if(localStorage.getItem('memo') == null) {
+  localStorage.setItem('memo', "");
+};
+
+// メモを編集するための関数
+function edit() {
+  document.getElementById("memo").addEventListener('keypress', enter);  // id memo に対して、特定のキーボード操作を可能に
+  function enter(e) {
+    if (e.keyCode === 13) { // enter が入力された場合
+      // local storage の memo をキーとする部分に変更後の内容を追加
+      const contents = document.getElementById("memo").value;
+      localStorage.setItem('memo', contents);
+      return false;
+    }  
+  }
+};
